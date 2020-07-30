02aa2cd3e56069588b4942d1be5b7e4b23810d03
yashw123 <66122455+yashw123@users.noreply.github.com>
2020-07-30 15:04:39 +0530

    Changes_1

---
+++ Sample_Git/Assets/Cube_Color.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cube_Color : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Sample_Git/Assets/Cube_rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_rotate : MonoBehaviour
+{   public float spinForce;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+    transform.Rotate(0,spinForce*Time.deltaTime,0);    
+    }
+	public void ChangeSpin()
+	{
+	spinForce=-spinForce;
+
+	}
+}
